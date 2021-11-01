7340afe18fa6f2d969b9a399e650b56b9a28c49a
Pete Parker <peter.bee.parker@gmail.com>
2021-11-01 09:44:23 -0600

    Add "random" mode and simplify button layout

--- src/App.js
+++ src/App.js
@@ -15,22 +15,40 @@ function getSubNumbers(num, length) {
 function App() {
   const [target, updateTarget] = useState(16);
   const [currentValue, updateCurrentValue] = useState(1);
+  const [selected, updateSelected] = useState(-1);
+  const [mode, changeMode] = useState("normal");
   const digits = getSubNumbers(currentValue, target.toString().length - 1);
 
-  if (currentValue === target) updateTarget((p) => p + 16);
+  if (currentValue === target) updateTarget(
+    (p) => p + (mode === "normal" ? 16 : Math.ceil(Math.random() * p))
+  );
+
+  const changeValue = (direction = 1) => {
+    const selectedValue = digits[selected];
+    if (selectedValue) {
+      updateCurrentValue(p => p + selectedValue * direction);
+      updateSelected(-1);
+    }
+  }
 
   return (
     <div className="App">
+      <div
+        onClick={() => changeMode((p) => (p === "normal" ? "random" : "normal"))}
+        style={{textTransform: "uppercase", position: "absolute", right: 10, top: 10}}
+      >
+        {mode}
+      </div>
       <h3>Goal: {target}</h3>
       <h1 style={{background: "plum", border: "1px solid purple", borderWidth: "1px 0", color: "purple"}}>{currentValue}</h1>
-      <h3>Add:&nbsp;
-      {digits.map((v, i) => 
-        <button key={i} onClick={() => updateCurrentValue(p => p + v)}>{v}</button>
-      )}</h3>
-      <h3>Subtract:&nbsp;
-      {digits.map((v, i) => 
-        <button key={i} onClick={() => updateCurrentValue(p => p - v)}>{v}</button>
-      )}</h3>
+      <div>{digits.map((v, i) => 
+        <button key={i} className={selected === i ? 'selected' : ''} onClick={() => updateSelected(i)}>{v}</button>
+      )}</div>
+      {selected > -1 && 
+      <>
+        <button onClick={() => changeValue(1)}>+</button>
+        <button onClick={() => changeValue(-1)}>&#8722;</button>
+      </>}
     </div>
   );
 }
